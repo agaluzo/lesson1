125ebdab87867b73a795cff540bd642c7a02c76c
agaluzo <vaalex@tut.by>
2016-09-22 15:49:17 +0300

    Add files via upload

---
+++ l1_1.py
@@ -0,0 +1,15 @@
+print ("1. Soup and salad")
+print ("2. Pasta with meat sauce")
+print ("3. Chef's special")
+number = input('Which number would you like to order?')
+if number == str(1):
+   print("One Soup and salad coming right up!")
+elif number == str(2):
+    print("One Pasta with meat sauce coming right up!")
+elif number == str(3):
+    print("One Chef's special coming right up!")
+else:
+    print("Sorry, that is not a valid choice.")
+
+
+
---
+++ l1_3.py
@@ -0,0 +1,18 @@
+MinFixedMonthlyPayment = 10
+balance = 3926
+annualInterestRate = 0.2
+unpaidBalance = updatedBalance = balance
+totalPaid = 0
+
+while True:
+    for i in range (1, 13):
+        unpaidBalance = updatedBalance - MinFixedMonthlyPayment
+        interest = annualInterestRate/12 * unpaidBalance
+        updatedBalance = unpaidBalance + interest
+    if unpaidBalance > 0:
+        updatedBalance = balance
+        MinFixedMonthlyPayment += 10
+    else:
+        break
+
+print('Lowest payment: %d' % MinFixedMonthlyPayment)
