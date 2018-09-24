7968fa376668ef6110e017402be3fb83dd436625
agaluzo <vaalex@tut.by>
2018-09-24 13:22:10 +0300

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,17 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+
+def static "swipe.jsSwipe.swipeDirection"(
+    	String direction	) {
+    (new swipe.jsSwipe()).swipeDirection(
+        	direction)
+}
+
+def static "swipe.jsSwipe.getCurrentSessionMobileDriver"() {
+    (new swipe.jsSwipe()).getCurrentSessionMobileDriver()
+}
